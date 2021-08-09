ac51dbea66187485426848590745fb633d246122
mkushcheva <mkushcheva@diasoft.ru>
2021-08-09 09:31:14 +0300

    Вынесла имя файла csv в конфиг

--- students-test/src/main/java/ru/otus/spring/Main.java
+++ students-test/src/main/java/ru/otus/spring/Main.java
@@ -4,18 +4,33 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.otus.spring.service.QuestionService;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("/spring-context.xml");
-        File source = context.getResource("question.csv").getFile();
+        Properties property = new Properties();
+        FileInputStream fis;
 
-        // получить сервис с Вопросами
-        QuestionService service = context.getBean(QuestionService.class);
-        // вывести список вопросов
-        service.printQuestion(source);
-        context.close();
+        try {
+            fis = new FileInputStream("src/main/resources/config.properties");
+            property.load(fis);
+            String csvFileName = property.getProperty("csvFileName");
+
+            File source = context.getResource(csvFileName).getFile();
+
+            // получить сервис с Вопросами
+            QuestionService service = context.getBean(QuestionService.class);
+            // вывести список вопросов
+            service.printQuestion(source);
+            context.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
