5866c3c7e60531fa8cbf0473382a4dd21dc432c3
mkushcheva <mkushcheva@diasoft.ru>
2021-08-09 11:34:30 +0300

    сделать чтение ресурса в бине

--- students-test/src/main/java/ru/otus/spring/Main.java
+++ students-test/src/main/java/ru/otus/spring/Main.java
@@ -1,32 +1,25 @@
 package ru.otus.spring;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.Resource;
+import ru.otus.spring.dao.QuestionResource;
 import ru.otus.spring.service.QuestionService;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Properties;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("/spring-context.xml");
-        Properties property = new Properties();
-        FileInputStream fis;
+        QuestionResource questionResource = context.getBean(QuestionResource.class);
+        Resource resource = questionResource.getResource();
 
         try {
-            fis = new FileInputStream("src/main/resources/config.properties");
-            property.load(fis);
-            String csvFileName = property.getProperty("csvFileName");
-
-            File source = context.getResource(csvFileName).getFile();
-
             // получить сервис с Вопросами
             QuestionService service = context.getBean(QuestionService.class);
             // вывести список вопросов
-            service.printQuestion(source);
+            service.printQuestion(resource.getFile());
             context.close();
 
         } catch (IOException e) {
--- students-test/src/main/java/ru/otus/spring/dao/QuestionDao.java
+++ students-test/src/main/java/ru/otus/spring/dao/QuestionDao.java
@@ -3,7 +3,6 @@ package ru.otus.spring.dao;
 import ru.otus.spring.domain.Question;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.List;
 
 public interface QuestionDao {
