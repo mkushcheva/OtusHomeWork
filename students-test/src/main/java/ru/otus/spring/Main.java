0757dbfcad83bbb80bbe3f7264be0c012b74a079
mkushcheva <mkushcheva@diasoft.ru>
2021-09-14 09:23:57 +0300

    убрать Exception

--- students-test/src/main/java/ru/otus/spring/Main.java
+++ students-test/src/main/java/ru/otus/spring/Main.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("/spring-context.xml");
         QuestionResource questionResource = context.getBean(QuestionResource.class);
